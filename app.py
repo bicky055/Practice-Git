854ceec85740b5f015765454688ab79045cb1ec6
Bicky Jha <77110980+bicky055@users.noreply.github.com>
2025-05-27 15:58:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ def adition(a,b):
     return a + b
 def subtraction(a,b):
     return a - b
-def multiplication(a,b):
-    return a * b     
+def multiplication(a,b,c):
+    return a * b * c    
 def division(a,b):
     if b == 0:
         return "Error: Division by zero"
